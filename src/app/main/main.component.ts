7f33a2907f1e1de52377a8fb55dd6f62bd9869f7
keshavkagrawal28 <keshavkagrawal28@gmail.com>
2024-01-23 15:53:48 +0530

    Added drag and drop

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+  CdkDrag,
+  CdkDropList,
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [CdkDropList, CdkDrag],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+  styleUrl: './main.component.scss',
 })
 export class MainComponent {
+  todo = ['Get up', 'Brush teeth', 'Take a shower', 'Go home'];
+  inProgress = ['Get to work', 'Pick up groceries', 'Fall asleep'];
+  review = ['Check e-mail'];
+  done = ['Walk dog'];
 
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
 }
